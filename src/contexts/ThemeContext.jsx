dbc36621e7471ac5b7049bcf575f1a29ae66f283
toxicgipsy <amanarazmedov@gmail.com>
2024-07-08 00:45:49 +0300

    context

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -2,32 +2,31 @@ import { createContext, useEffect, useState } from "react";
 
 export const ThemesContext = createContext(null);
 
-const checkLS = () => {
-    try {
-      const theme = JSON.parse(localStorage.getItem("theme"));
-      return theme;
-    } catch(error) {
-      console.log(error.message);
-      localStorage.removeItem("theme");
-      return null;
-    }
+const getThemeLocalStorage = () => {
+  try {
+    const theme = JSON.parse(localStorage.getItem("theme"));
+    return theme;
+  } catch (error) {
+    console.log(error.message);
+    localStorage.removeItem("theme");
+    return null;
   }
-export const ThemesProvider = ({children}) => {
+};
+export const ThemesProvider = ({ children }) => {
+  const [theme, setTheme] = useState(getThemeLocalStorage());
 
-    const [theme, setTheme] = useState(checkLS());
+  useEffect(() => {
+    handleTheme();
+  }, []);
 
-    useEffect(() => {
-        handleTheme();
-      }, []);
-      
-    const handleTheme = () => {
-        theme ? setTheme(false) : setTheme(true);
-        localStorage.setItem("theme", JSON.stringify(theme));
-      }
+  const handleTheme = () => {
+    theme ? setTheme(false) : setTheme(true);
+    localStorage.setItem("theme", JSON.stringify(theme));
+  };
 
-    return (
-        <ThemesContext.Provider value={{theme, setTheme, handleTheme}}>
-            {children}
-        </ThemesContext.Provider>
-    )
-}
+  return (
+    <ThemesContext.Provider value={{ theme, setTheme, handleTheme }}>
+      {children}
+    </ThemesContext.Provider>
+  );
+};
