fb7f5dd65e745a6423414732e845bf3ab27fd024
Aman Arazmedov <aman@example.com>
2026-06-07 17:56:42 +0300

    separation into components

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,35 @@
 // import { useState } from 'react'
-import logo from '../public/logo.png'
-import logo_dark from '../public/logo_dark.png'
-import './App.css'
+import "./App.css";
+import { Header } from "./components/Header.jsx";
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-        <div className="wrapper">
-          {/* pop-up start */}
-            <div className="pop-exit" id="popExit">
-                <div className="pop-exit__container">
-                    <div className="pop-exit__block">
-                        <div className="pop-exit__ttl">
-                            <h2>Выйти из аккаунта?</h2>
-                        </div>
-                        <form className="pop-exit__form" id="formExit" action="#">
-                            <div className="pop-exit__form-group">
-                                <button className="pop-exit__exit-yes _hover01" id="exitYes"><a href="modal/signin.html">Да, выйти</a> </button>
-                                <button className="pop-exit__exit-no _hover03" id="exitNo"><a href="main.html">Нет, остаться</a> </button>
-                            </div>
-                        </form>
-                    </div>
+      <div className="wrapper">
+        {/* pop-up start */}
+        <div className="pop-exit" id="popExit">
+          <div className="pop-exit__container">
+            <div className="pop-exit__block">
+              <div className="pop-exit__ttl">
+                <h2>Выйти из аккаунта?</h2>
+              </div>
+              <form className="pop-exit__form" id="formExit" action="#">
+                <div className="pop-exit__form-group">
+                  <button className="pop-exit__exit-yes _hover01" id="exitYes">
+                    <a href="modal/signin.html">Да, выйти</a>{" "}
+                  </button>
+                  <button className="pop-exit__exit-no _hover03" id="exitNo">
+                    <a href="main.html">Нет, остаться</a>{" "}
+                  </button>
                 </div>
+              </form>
             </div>
+          </div>
+        </div>
 
-            <div className="pop-new-card" id="popNewCard">
+        {/* <div className="pop-new-card" id="popNewCard">
                 <div className="pop-new-card__container">
                     <div className="pop-new-card__block">
                         <div className="pop-new-card__content">
@@ -135,9 +138,9 @@ function App() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className="pop-browse" id="popBrowse">
+        {/* <div className="pop-browse" id="popBrowse">
                 <div className="pop-browse__container">
                     <div className="pop-browse__block">
                         <div className="pop-browse__content">
@@ -273,36 +276,58 @@ function App() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-          {/* pop-up end */}
-            <header className="header">
-                <div className="container">
-                    <div className="header__block">
-                        <div className="header__logo _show _light">
-                            <a href="" target="_self"><img src={logo} classNameName="vite" alt="Vite logo" /></a>
-                        </div>
-                        <div className="header__logo _dark">
-                            <a href="" target="_self"><img src={logo_dark} classNameName="vite" alt="Vite logo" /></a>
-                        </div>
-                        <nav className="header__nav">
-                            <button className="header__btn-main-new _hover01" id="btnMainNew"><a href="#popNewCard">Создать новую задачу</a></button>
-                            <a href="#user-set-target" className="header__user _hover02">Ivan Ivanov</a>
-                            <div className="header__pop-user-set pop-user-set" id="user-set-target">
-                                {/* <a href="">x</a> */}
-                                <p className="pop-user-set__name">Ivan Ivanov</p>
-                                <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-                                <div className="pop-user-set__theme">
-                                    <p>Темная тема</p>
-                                    <input type="checkbox" className="checkbox" name="checkbox"/>
-                                </div>
-                                <button type="button" className="_hover03"><a href="#popExit">Выйти</a></button>
-                            </div>
-                        </nav>
-                    </div>
+        {/* pop-up end */}
+
+        <Header />
+        {/* <header className="header">
+          <div className="container">
+            <div className="header__block">
+              <div className="header__logo _show _light">
+                <a href="" target="_self">
+                  <img src={logo} classNameName="vite" alt="Vite logo" />
+                </a>
+              </div>
+              <div className="header__logo _dark">
+                <a href="" target="_self">
+                  <img src={logo_dark} classNameName="vite" alt="Vite logo" />
+                </a>
+              </div>
+              <nav className="header__nav">
+                <button
+                  className="header__btn-main-new _hover01"
+                  id="btnMainNew"
+                >
+                  <a href="#popNewCard">Создать новую задачу</a>
+                </button>
+                <a href="#user-set-target" className="header__user _hover02">
+                  Ivan Ivanov
+                </a>
+                <div
+                  className="header__pop-user-set pop-user-set"
+                  id="user-set-target"
+                >
+                  <p className="pop-user-set__name">Ivan Ivanov</p>
+                  <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
+                  <div className="pop-user-set__theme">
+                    <p>Темная тема</p>
+                    <input
+                      type="checkbox"
+                      className="checkbox"
+                      name="checkbox"
+                    />
+                  </div>
+                  <button type="button" className="_hover03">
+                    <a href="#popExit">Выйти</a>
+                  </button>
                 </div>
-            </header>
-            <main className="main">
+              </nav>
+            </div>
+          </div>
+        </header> */}
+
+        {/* <main className="main">
                 <div className="container">
 
                     <div className="main__block">
@@ -739,11 +764,10 @@ function App() {
 
                     </div>
                 </div>
-            </main>
-
-        </div>
+            </main> */}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
