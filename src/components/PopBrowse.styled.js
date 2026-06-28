ba541b455b9edfc2a0b0c21f1d977200c6158f07
Aman Arazmedov <aman@example.com>
2026-06-28 22:33:17 +0300

    add style component popUser

--- src/components/PopBrowse.styled.js
+++ src/components/PopBrowse.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const SPopBrowseWrapper = styled.div`
-  /* display: ${({ $isOpen }) => ($isOpen ? "block" : "none")}; */
+  display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
   width: 100%;
   height: 100%;
   min-width: 375px;
--- src/components/PopExit.jsx
+++ src/components/PopExit.jsx
@@ -1,26 +1,36 @@
+import {
+  SPopExitBlock,
+  SPopExitContainer,
+  SPopExitFormGroup,
+  SPopExitNo,
+  SPopExitNoA,
+  SPopExitTtl,
+  SPopExitWrapper,
+  SPopExitYes,
+  SPopExitYesA,
+} from "./PopExit.styled";
+
 function PopExit() {
   return (
-    <>
-      <div className="pop-exit" id="popExit">
-        <div className="pop-exit__container">
-          <div className="pop-exit__block">
-            <div className="pop-exit__ttl">
-              <h2>Выйти из аккаунта?</h2>
-            </div>
-            <form className="pop-exit__form" id="formExit" action="#">
-              <div className="pop-exit__form-group">
-                <button className="pop-exit__exit-yes _hover01" id="exitYes">
-                  <a href="modal/signin.html">Да, выйти</a>{" "}
-                </button>
-                <button className="pop-exit__exit-no _hover03" id="exitNo">
-                  <a href="main.html">Нет, остаться</a>{" "}
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </>
+    <SPopExitWrapper id="popExit">
+      <SPopExitContainer>
+        <SPopExitBlock>
+          <SPopExitTtl>
+            <h2>Выйти из аккаунта?</h2>
+          </SPopExitTtl>
+          <form className="pop-exit__form" id="formExit" action="#">
+            <SPopExitFormGroup>
+              <SPopExitYes id="exitYes">
+                <SPopExitYesA href="modal/signin.html">Да, выйти</SPopExitYesA>
+              </SPopExitYes>
+              <SPopExitNo id="exitNo">
+                <SPopExitNoA href="main.html">Нет, остаться</SPopExitNoA>
+              </SPopExitNo>
+            </SPopExitFormGroup>
+          </form>
+        </SPopExitBlock>
+      </SPopExitContainer>
+    </SPopExitWrapper>
   );
 }
 
