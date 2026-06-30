f15e2d04005169ed073f2e587075aa0cf33fed1b
Aman Arazmedov <aman@example.com>
2026-06-30 15:12:30 +0300

    del import css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import PopBrowse from "./components/PopBrowse";
