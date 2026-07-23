ef82c1b045a5eb529ceeb67da1d289436e2dbeb6
Aman Arazmedov <aman@example.com>
2026-07-23 21:17:50 +0300

    add contextAPI and context logic App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
+import { useState } from "react";
 import AppRoutes from "./components/AppRoutes";
+import { AuthContext, TaskContext } from "./context/contextAPI";
 import { GlobalStyle } from "./styles/GlobalStyle";
 
 function App() {
+  const [auth, setAuth] = useState(null);
+  const [tasks, setTasks] = useState([]);
+
+  const loginUser = (loginData) => {
+    setAuth(loginData);
+  };
+
+  const logoutUser = () => {
+    setAuth(null);
+  };
+
+  const addTask = (newTask) => {
+    setTasks((prevTasks) => [...prevTasks, newTask]);
+  };
+
+  const updateTask = (taskId, changes) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === taskId ? { ...task, ...changes } : task,
+      ),
+    );
+  };
+
+  const deleteTask = (taskId) => {
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
+  };
+
+  const authValue = {
+    auth,
+    isAuth: Boolean(auth),
+    loginUser,
+    logoutUser,
+  };
+
+  const taskValue = {
+    tasks,
+    addTask,
+    updateTask,
+    deleteTask,
+  };
+
   return (
     <>
       <GlobalStyle />
-      <AppRoutes />
+      <AuthContext.Provider value={authValue}>
+        <TaskContext.Provider value={taskValue}>
+          <AppRoutes />
+        </TaskContext.Provider>
+      </AuthContext.Provider>
     </>
   );
 }
